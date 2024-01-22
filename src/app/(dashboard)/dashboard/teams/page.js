5d3ff6d1649b2a1a7618ff7e51dcf5aad4daef1d
Karthik <karthithelearner@gmail.com>
2024-01-22 17:58:49 +0530

    all- dynamic

--- src/app/(dashboard)/dashboard/teams/page.js
+++ src/app/(dashboard)/dashboard/teams/page.js
@@ -3,7 +3,7 @@ import React from "react";
 
 async function TeamsPage() {
   const { data } = await getAllTeams();
-  console.log(data);
+  //console.log(data);
   return (
     <div className="w-full max-w-5xl mx-auto">
       <div className="flex flex-col">
@@ -11,17 +11,18 @@ async function TeamsPage() {
           All Teams
         </div>
         <div className="flex flex-row justify-evenly flex-wrap m-auto gap-5">
-          {data.map((team) => (
-            <div
-              key={team.id}
-              className="w-[200px] h-[200px] rounded-md shadow-lg border flex flex-col justify-center items-center"
-            >
-              <div className="w-[150px] h-[150px]">
-                <img src={team.logo} alt={team.shortName} />
+          {data &&
+            data.map((team) => (
+              <div
+                key={team.id}
+                className="w-[200px] h-[200px] rounded-md shadow-lg border flex flex-col justify-center items-center"
+              >
+                <div className="w-[150px] h-[150px]">
+                  <img src={team.logo} alt={team.shortName} />
+                </div>
+                <div className="text-center">{team.fullName}</div>
               </div>
-              <div className="text-center">{team.fullName}</div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </div>
--- src/app/api/team/getallteams/route.js
+++ src/app/api/team/getallteams/route.js
@@ -2,6 +2,8 @@ import cloudinary from "@/lib/cloudinary";
 import prisma from "@/lib/prismaclient";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   try {
     const allteams = await prisma.IPLTeams.findMany({});
